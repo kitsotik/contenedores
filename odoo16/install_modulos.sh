15f2437417211c2a459da5eed431230217008ac9
kitsotik <gonchibud@gmail.com>
2024-08-03 14:14:06 -0300

    Actualizacion realizada el 2024-08-03 14:14:06

--- odoo16/install_modulos.sh
+++ odoo16/install_modulos.sh
@@ -2,5 +2,5 @@
 
 sudo docker exec -it -u root odoo16-app sh /mnt/scripts/updateupgrade.sh
 sudo docker exec -it -u root odoo16-app sh /mnt/scripts/modulos_extra.sh
-sudo docker exec -it -u root odoo16-app sh /mnt/scripts/modulos_l10n_ar.sh
+sudo docker exec -it -u root odoo16-app sh /mnt/scripts/modulos_l10n_ar_adhoc.sh
 sudo docker exec -it -u root odoo16-app sh /mnt/scripts/modulos_oca.sh
